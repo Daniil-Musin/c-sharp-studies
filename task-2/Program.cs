4a6ceeec8a62e197134a9294d095a257ad421ae4
Daniil.Musin <Daniil.Musin@urfu.me>
2021-09-30 13:14:51 +0500

    release 1.0.0

---
+++ task-2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Hi there! I'm the Square calculator
+Enter the side's length of your square in integer:");
+
+            int squareLength = int.Parse(Console.ReadLine());
+            PerimeterCalculator(squareLength);
+            AreaCalculator(squareLength);
+            Console.WriteLine(
+                "The perimeter is " +
+                PerimeterCalculator(squareLength) +
+                " and the area is " +
+                AreaCalculator(squareLength)
+                );
+            Console.WriteLine(@"
+////Type to exit");
+            Console.ReadKey();
+
+        }
+
+        static int PerimeterCalculator(int arg)
+        {
+            return arg * 4;
+        }
+
+        static int AreaCalculator(int arg)
+        {
+            return arg * arg;
+        }
+    }
+}
