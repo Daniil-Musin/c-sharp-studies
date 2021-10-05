62c631a2f091545946a1134a523482f3bdd2457b
Daniil.Musin <Daniil.Musin@urfu.me>
2021-10-05 16:27:53 +0500

    v.1.0

---
+++ task-3/task-3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, user!");
+            Console.WriteLine("Enter your x, please:");
+            var x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"This is what you get: {EquationSolver(x)}");
+            Console.WriteLine("////Type to exit");
+            Console.ReadKey();
+        }
+
+        static double EquationSolver(double x)
+        {
+            return 
+                2 * Math.Cos(1 / Math.Pow(x, 2.0))
+                / (Math.Pow(x, 3.0) + 1);
+        }
+    }
+}
