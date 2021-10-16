af6677687f1593e218c9bbc4822a5333b1e61f36
Daniil.Musin <Daniil.Musin@urfu.me>
2021-10-16 20:55:20 +0500

    task-4-release

---
+++ task-4/task-4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, user!");
+            FullEquationCalculator();
+            Console.WriteLine("/// type to exit");
+            Console.ReadKey();
+        }
+
+        static void FullEquationCalculator() 
+        {
+            double answer = smallCalc(3, 5) * smallCalc(3, 7) * smallCalc(5, 5);
+            Console.WriteLine($"Your answer is {Math.Round(answer, 2)}");
+        }
+
+        static double smallCalc(double arg1, double arg2)
+        {
+            return (Math.Sin(arg1) + Math.Sqrt(arg1)) / (Math.Cos(arg2) + Math.Sqrt(arg2));
+        }
+    }
+}
