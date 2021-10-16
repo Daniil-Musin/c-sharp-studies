d69eb659ef5fabc021fd16a890d460200d8d9fa9
Daniil.Musin <Daniil.Musin@urfu.me>
2021-10-16 20:55:20 +0500

    fix 1.0.1

---
+++ task-4/task-4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, user!");
+            Console.WriteLine($"Your answer is {CalculateFullExpression()}"); ;
+            Console.WriteLine("/// type to exit");
+            Console.ReadKey();
+        }
+
+        static double CalculateFullExpression() 
+        {
+            double answer = CalculateSmallerPart(3, 5) * CalculateSmallerPart(3, 7) * CalculateSmallerPart(5, 5);
+            return Math.Round(answer, 2);
+        }
+
+        static double CalculateSmallerPart(double a, double b)
+        {
+            return (Math.Sin(a) + Math.Sqrt(a)) / (Math.Cos(b) + Math.Sqrt(b));
+        }
+    }
+}
