247891eedcfac744c875bd1d21adcac44011cda1
Daniil.Musin@urfu.me <Daniil.Musin@urfu.me>
2021-10-18 13:40:09 +0500

    task 5.1 and task 5.2

---
+++ Maths/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Maths
+{
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+            int justNumber = 2;
+            const double gravityAcceleration = 9.8;
+            
+            Console.WriteLine(justNumber);
+            Console.WriteLine(justNumber++);
+            Console.WriteLine(++justNumber);
+            Console.WriteLine(gravityAcceleration);
+            Console.WriteLine(double.MaxValue);
+            Console.ReadKey();
+            var l = 0x91;
+            Console.WriteLine(l.GetType());
+
+
+        }
+    }
+}
