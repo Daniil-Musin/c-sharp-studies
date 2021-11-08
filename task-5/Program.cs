6068cf5a22c8409a337e5d8aef82259f2ebd13e1
Daniil.Musin <Daniil.Musin@urfu.me>
2021-11-08 18:04:31 +0500

    task-5-release

---
+++ task-5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace task_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Hello user! This is a TRANSLIT app
+Enter your text, please:");
+            Console.WriteLine(Transliterate(Console.ReadLine()));
+            Console.WriteLine("/// Type to exit");
+            Console.ReadKey();
+        }
+
+        static string Transliterate(string str)
+        {
+
+            string[] latinDictionary = { "A", "B", "V", "G", "D", "E", "Yo", "Zh", "Z", "I", "Y", "K", "L", "M", "N", "O", "P", "R", "S", "T", "U", "F", "KH", "TS", "CH", "SH", "SHCH", "IE", "Y", "", "E", "YU", "YA" };
+            string[] russianDictionary = { "А", "Б", "В", "Г", "Д", "Е", "Ё", "Ж", "З", "И", "Й", "К", "Л", "М", "Н", "О", "П", "Р", "С", "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ы", "Ь", "Э", "Ю", "Я" };
+
+            for (int i = 0; i <= 32; i++)
+            {
+                str = str.ToUpper();
+                str = str.Replace(russianDictionary[i], latinDictionary[i]);
+                //str = str.Replace(rus_low[i], lat_low[i]);
+            }
+            return str;
+        }
+    }
+}
