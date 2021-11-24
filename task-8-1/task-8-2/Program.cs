4e1949798368711b09000e4f6c5b3bf543c44611
Daniil.Musin <Daniil.Musin@urfu.me>
2021-11-24 12:46:28 +0500

    8.2

---
+++ task-8-1/task-8-2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace task_8_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Hello user!\nHow much numbers do you want to enter?: ");
+                int quantityOfNumbers = int.Parse(Console.ReadLine());
+                Console.WriteLine($"{CalculateProductOfNumbersGiven(quantityOfNumbers)}");
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error.Message);
+            }
+            Console.ReadKey();
+        }
+
+        static double CalculateProductOfNumbersGiven(int cycles)
+        {
+            Console.Write("Enter your first number: ");
+            double answer = double.Parse(Console.ReadLine());
+            for (int i = 1; i < cycles; i++)
+            {
+                Console.Write($"Enter number {i + 1}: ");
+                answer *= double.Parse(Console.ReadLine());
+            }
+            return answer;
+        }
+    }
+}
