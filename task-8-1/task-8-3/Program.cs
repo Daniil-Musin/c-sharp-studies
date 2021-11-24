caa5670cddae3eeb0d9923cc7aafeff3ea9a2382
Daniil.Musin <Daniil.Musin@urfu.me>
2021-11-24 13:14:29 +0500

    8.3

---
+++ task-8-1/task-8-3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace task_8_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Hello user!\nEnter your a (1 < a < 1,5): ");
+                double a = double.Parse(Console.ReadLine());
+                if (!(a > 1 & a < 1.5))
+                    throw new Exception("out of given range");
+                Console.WriteLine("Your numbers out of that a: ");
+                PrintNumbers(a);
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error.Message);
+            }
+
+            Console.ReadKey();
+        }
+
+        static void PrintNumbers(double limit)
+        {
+            for (double n = 2; 1 + 1 / n >= limit; n++)
+            {
+                Console.WriteLine(1 + 1 / n);
+            }
+        }
+    }
+}
