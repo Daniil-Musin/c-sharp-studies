7e4b75c4f6231dcfcc3cf1bbb6c46390bc91aefd
Daniil.Musin <Daniil.Musin@urfu.me>
2021-11-24 14:16:53 +0500

    8.4

---
+++ task-8-1/task-8-4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace task_8_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Hello user!\nEnter your integer: ");
+                int number = int.Parse(Console.ReadLine());
+                ShowMaxAndMinDigits(number);
+
+                Console.ReadKey();
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error.Message);
+            }
+        }
+        static void ShowMaxAndMinDigits(int number)
+        {
+            
+            for (int i = 9; i > -1; i--)
+            {
+                if (Convert.ToString(number).Contains(Convert.ToString(i)))
+                {
+                    Console.WriteLine($"Maximum digit is: {i}");
+                    break;
+                }
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (Convert.ToString(number).Contains(Convert.ToString(i)))
+                {
+                    Console.WriteLine($"Minimum digit is: {i}");
+                    break;
+                }
+            }
+
+        }
+    }
+}
