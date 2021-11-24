c80d87e44e0ef72eb07f4034183863fde0ef0ad8
Daniil.Musin <Daniil.Musin@urfu.me>
2021-11-24 12:13:15 +0500

    8.1

---
+++ task-8-1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace task_8_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Hello user!\nEnter your n: ");
+                int n = int.Parse(Console.ReadLine());
+                Console.Write("Enter your x: ");
+                double x = double.Parse(Console.ReadLine());
+                Console.WriteLine($"Your answer: {CalculateSum(n, x)}");
+
+                Console.ReadKey();
+            }
+            catch (Exception error)
+            {
+
+                Console.WriteLine($"!!@@{error.Message}@@!!");
+            }
+        }
+
+        static double CalculateSum(int n, double x)
+        {
+            double answer = 1;
+            for (int i = 1; i < n + 1; i++)
+            {
+                answer += (double)(i + 1) / (double)(i + 2) * Math.Pow(x, i);
+            }
+            
+            return answer;
+        }
+    }
+}
