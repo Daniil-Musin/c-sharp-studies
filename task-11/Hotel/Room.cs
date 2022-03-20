147f1ac3cdbfd2ed791b0ab55c8aaf4a73e76b60
LalaTheAlter <musin.daniiil@yandex.ru>
2022-03-20 20:30:28 +0500

    Все тесты зелёные

---
+++ task-11/Hotel/Room.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Hotel
+{
+    public class Room
+    {
+        public readonly int Number;
+        public int BedCount;
+        public readonly Direction WindowsDirection;
+        public double CostPerNight { get; set; }
+        public DateTime TimeOfLeaving { get; set; }
+
+        public Room(int number, int bedCount, Direction windowsDirection)
+        {
+            Number = number;
+            BedCount = bedCount;
+            WindowsDirection = windowsDirection;
+            
+        }
+
+
+        public override string ToString()
+        {
+            return $"Room #{Number}";
+                
+        }
+
+    }
+}
